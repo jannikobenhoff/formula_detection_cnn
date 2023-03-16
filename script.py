db846b92cb8130202d35fce85c8ca060e8e4ae23
Jannik Obenhoff <hello@jannikobenhoff.com>
2023-03-16 20:10:34 +0100

    report finished

--- script.py
+++ script.py
@@ -3,6 +3,10 @@ if __name__ == "__main__":
     1. Dataset downloaden
     2. unzippen
     3. in /utilities/__files/ ablegen
+    Die nächsten Schritte wären:
+    - In Test/Train Ordner aufsplitten
+    - weitere preprocessing wie im Report beschrieben auf alle Bilder anwenden
+    bei Bedarf können wir das gerne auch nochmal genauer erklären.
     """
     url = "https://www.kaggle.com/datasets/xainano/handwrittenmathsymbols/download"
 
