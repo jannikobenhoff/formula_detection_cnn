03c9692d4879b49b9f8ab49780df5aa675c3a34c
Jannik Obenhoff <hello@jannikobenhoff.com>
2023-03-16 20:06:46 +0100

    report finished

---
+++ script.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    """
+    1. Dataset downloaden
+    2. unzippen
+    3. in /utilities/__files/ ablegen
+    """
+    url = "https://www.kaggle.com/datasets/xainano/handwrittenmathsymbols/download"
+
+
+
+
